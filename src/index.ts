ad792f5426df6780ca0891b6ed79d0efc447a3e6
Alexander Snov <snov.contact@gmail.com>
2021-03-18 02:52:10 +0700

    fix: export connectors

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export type { BaseEvent, EventCreator } from './Events';
 export { makeEventCreator, eventFactoryWrapper } from './Events';
+
+export type { BaseConnector } from './Connector.h';
+export { ClosedConnector } from './ClosedConnector';
