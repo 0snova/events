aa7d0633509192e733e598c9bab7e303c8cff934
Alexander Snov <snov.contact@gmail.com>
2021-03-18 01:57:26 +0700

    test: clean test

--- src/Events.spec.ts
+++ src/Events.spec.ts
@@ -1,12 +1,11 @@
 import { makeEventCreator } from './Events';
 
 test(`event type is injected`, () => {
+  const EVENT_TYPE = 'EVENT_TYPE' as const;
+  type EventType = typeof EVENT_TYPE;
   type Payload = {
     value: number;
   };
-
-  const EVENT_TYPE = 'EVENT_TYPE' as const;
-  type EventType = typeof EVENT_TYPE;
 
   const event = makeEventCreator<Payload, EventType>(EVENT_TYPE)({ value: 420 });
 
@@ -20,8 +19,6 @@ test(`payload is injected into the event`, () => {
   const payload: EventPayload = { value: 420 };
 
   const event = makeEventCreator('EVENT_TYPE')(payload);
-
-  console.log(event);
 
   expect(event.payload).toEqual(payload);
 });
