069f5e424214c7b50f8b8a360e2a55a3e92c3132
Alexander Snov <snov.contact@gmail.com>
2021-03-18 02:38:34 +0700

    chore: remove env output

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,10 @@
 module.exports = (api) => {
   const isTest = api.env('test');
 
-  if (isTest) {
-    console.log(`babel is using "test" enviroment`);
-  }
-
   return {
-    presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript'],
+    presets: [
+      ['@babel/preset-env', { targets: isTest ? { node: 'current' } : 'defaults' }],
+      '@babel/preset-typescript',
+    ],
   };
 };
