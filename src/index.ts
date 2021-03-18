6f946dbadb44416feec0fcf103e83e222bd4603f
Alexander Snov <snov.contact@gmail.com>
2021-03-18 23:22:24 +0700

    feat: export RequestConnector public API

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 export type { BaseEvent, EventCreator } from './Events';
 export { makeEventCreator, eventFactoryWrapper } from './Events';
 
-export type { InferEventCreatorsMap } from './EventMap';
+export type { InferEventFromCreatorsMap } from './EventMap';
 
 export type { BaseConnector } from './Connector.h';
 export { ClosedConnector } from './ClosedConnector';
+
+export type { RequestConnectorOptions, InferResponseEventMap } from './RequestConnector';
+export { makeResponseEvent, RequestConnector } from './RequestConnector';
