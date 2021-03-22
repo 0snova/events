2e29bb386ca8d7d7f60668c96fc7f75b3dc95de1
Alexander Snov <snov.contact@gmail.com>
2021-03-23 03:55:54 +0700

    chore: use @osnova/build-cli

--- scripts/prepare-files.js
+++
@@ -1,8 +0,0 @@
-const { copyFiles } = require('@osnova/build-library');
-const { makePackageJSON, savePackageJSON } = require('@osnova/build-library');
-
-copyFiles(['README.md']).catch((e) => {
-  console.error(e);
-});
-
-savePackageJSON(makePackageJSON(require('../package.json')));
