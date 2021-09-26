91acfc41793ee0409da1bd194757ee30ec6a66a8
Alexander Snov <snov.contact@gmail.com>
2021-09-26 19:11:34 +0700

    DataEvent is identifiable

--- src/EventData.ts
+++ src/EventData.ts
@@ -1,9 +1,10 @@
-import { AnyEvent, BaseEvent } from './Events';
+import { IdentifiableEvent } from '.';
+import { AnyEvent } from './Events';
 
-export type DataEvent<D = any, T extends string = string> = BaseEvent<{ value: D }, T>;
+export type DataEvent<D = any, T extends string = string> = IdentifiableEvent<{ value: D }, T>;
 
 export type CastToDataEvent<E extends AnyEvent> = DataEvent<E['payload'], E['type']>;
-export type UnwrapDataEvent<E extends DataEvent> = E extends DataEvent<infer P, infer T> ? BaseEvent<P, T> : E;
+export type UnwrapDataEvent<E extends DataEvent> = E extends DataEvent<infer P, infer T> ? IdentifiableEvent<P, T> : E;
 
 export function isDataEvent<E extends AnyEvent>(e: E) {
   return e.type.endsWith(`::data`);
