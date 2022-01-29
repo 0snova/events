84977aef37442c634d4991a7cff71de983748d91
Alexander Snov <snov.contact@gmail.com>
2022-01-30 02:09:55 +0700

    Add support for ports

--- src/WebWorkerConnector.ts
+++ src/WebWorkerConnector.ts
@@ -3,20 +3,32 @@ import { RequestEvent } from './EventRequest';
 import { AnyResponseEventMap } from './EventResponse';
 import { ResponseMap } from './ResponseConnector';
 
-export function makeWebWorkerPostMessageProvider<Event extends RequestEvent>(worker: Worker) {
+export function makeWebWorkerPostMessageProvider<Event extends RequestEvent>(worker: Worker, port?: MessagePort) {
   return {
     postMessage(message: Event) {
-      worker.postMessage({ event: message });
+      if (port) {
+        port.postMessage({ event: message });
+      } else {
+        worker.postMessage({ event: message });
+      }
     },
   };
 }
 
 export function propagateWebWorkerEvents<Event extends RequestEvent>(worker: Worker, target: BaseConnector<Event>) {
-  worker.addEventListener('message', (msg: any) => {
+  worker.addEventListener('message', (msg) => {
     if (msg.data.event) {
       target.accept(msg.data.event);
     }
   });
+}
+
+export function propagatePortEvents<Event extends RequestEvent>(port: MessagePort, target: BaseConnector<Event>) {
+  port.onmessage = (msg) => {
+    if (msg.data.event) {
+      target.accept(msg.data.event);
+    }
+  };
 }
 
 export function makeWebWorkerConnector<
@@ -27,16 +39,22 @@ export function makeWebWorkerConnector<
 >(
   worker: Worker,
   responses?: Partial<ResponseMap<RequestEvent, OutResponseEventMap>>,
-  options?: DuplexConnectorOptions<InResponseEventMap[keyof InResponseEventMap]>
+  options?: DuplexConnectorOptions<InResponseEventMap[keyof InResponseEventMap]> & {
+    port?: MessagePort;
+  }
 ) {
   const connector = new DuplexConnector<OutReqEvents, InReqEvents, OutResponseEventMap, InResponseEventMap>(
-    new PostMessageTransferer(makeWebWorkerPostMessageProvider(worker)),
-    new PostMessageTransferer(makeWebWorkerPostMessageProvider(worker)),
+    new PostMessageTransferer(makeWebWorkerPostMessageProvider(worker, options?.port)),
+    new PostMessageTransferer(makeWebWorkerPostMessageProvider(worker, options?.port)),
     responses,
     options
   );
 
-  propagateWebWorkerEvents(worker, connector);
+  if (options?.port) {
+    propagatePortEvents(options.port, connector);
+  } else {
+    propagateWebWorkerEvents(worker, connector);
+  }
 
   return { connector };
 }
